32693dd2c91fbd42aa5527e9bacdc66da26a4c65
yousufrahman <ryousuf766@gmail.com>
2026-04-30 15:03:55 +0600

    for deployment

--- src/app/allanimals/page.jsx
+++ src/app/allanimals/page.jsx
@@ -1,8 +1,7 @@
 
 
 const AllAnimalPage = async () => {
-    const res = await fetch('/public/CowData.json')
-    const AllCow = await res.json()
+    
     return (
         <div>
             <h2 className="text-2xl font-bold text-center">All Animal</h2>
@@ -10,7 +9,7 @@ const AllAnimalPage = async () => {
                 sort
             </div>
             <div>
-                 <h2>{AllCow.length}</h2>
+                 
             </div>
         </div>
     );
